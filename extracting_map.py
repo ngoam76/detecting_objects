65b8dc2f3e78c86b0a810f1af34628cf06411e82
ngoam76 <nabchau76@gmail.com>
2023-11-16 20:56:59 +0100

    Found 4 points, extracted map with html

--- extracting_map.py
+++ extracting_map.py
@@ -1,16 +1,20 @@
 import requests
 from urllib.parse import urlencode
 
+
 api_key = "AIzaSyD06zFaNvB9VR8RiUrtwkVr0RX0nWmlnY0" # to access google maps data
+
 # geocoding API
+# https://maps.googleapis.com/maps/api/geocode/json?address=1600+Amphitheatre+Parkway,+Mountain+View,+CA&key=YOUR_API_KEY
+
 def extract_lat_lng(address_or_postalcode, data_type = "json"):
-    endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}"
-    params = {"address": address_or_postalcode, "key": api_key} # dictionary
+    endpoint = f"https://maps.googleapis.com/maps/api/geocode/{data_type}" #string
+    params = {"address": address_or_postalcode, "key": api_key} # dictionary 
     url_params = urlencode(params) # encode dict into URL query string
     url = f"{endpoint}?{url_params}" # comebine two strings
     r = requests.get(url)
     if r.status_code not in range(200, 299):
-        return {}
+        return {} # dictionary
     latlng = {}
     try:
         latlng = r.json()["results"][0]["geometry"]["location"]
@@ -18,5 +22,19 @@ def extract_lat_lng(address_or_postalcode, data_type = "json"):
         pass
     return latlng.get("lat"), latlng.get("lng")
 
-print(extract_lat_lng("174 Hung Vuong, Hue, Vietnam"))
-#1600 Amphitheatre Parkway, Mountain View, CA
+# maps static API
+
+# https://maps.googleapis.com/maps/api/staticmap?center=40.714728,-73.998672&zoom=12&size=400x400&key=YOUR_API_KEY&signature=YOUR_SIGNATURE
+# specifying locations from the center of created 4-point polygon
+# creating paths based on these 4 points and display them on the created image of gg maps 
+
+def plot_map(center_coord, map_zoom, map_size):
+    endpoint = f"https://maps.googleapis.com/maps/api/staticmap" #string
+    params = {"center": center_coord, "zoom": map_zoom, "size": map_size, "key": api_key} # dictionary 
+    url_params = urlencode(params) # encode dict into URL query string
+    url = f"{endpoint}?{url_params}" # comebine two strings
+    return url
+
+
+
+
--- main.py
+++ main.py
@@ -1,1 +1,24 @@
-print("Hello World")
+import extracting_map
+from shapely import wkt
+from shapely.geometry import Polygon
+
+polygon_coords = []
+# for i in range(4):
+#     address = input(f"Enter the {i+1}. address: ")
+
+address = ["Augustenstraße 92-94, 80798 München", "Augustenstraße 7, 80333 München", "Brienner Str. 11, 80333 München", "Schellingstraße 30, 80799 München"]
+
+for i in address:
+    polygon_coords.append(extracting_map.extract_lat_lng(i))
+
+# finding center point of 4 coords 
+polygon = Polygon(polygon_coords) # a list
+poly_wkt = polygon.wkt
+center_point = wkt.loads(f"{poly_wkt}").centroid.wkt
+lat_lng = center_point.replace("(", ")").replace(")", "").split(" ")
+
+# define parameters for the function
+center = f"{lat_lng[1]},{lat_lng[2]}"
+zoom = "14"
+size = "400x400"
+print(extracting_map.plot_map(center, zoom, size))
