4f7caec94ee122249ce333caf1a9fd58d7046ecf
ge27rex <chau.nguyen-ai-bao@tum.de>
2023-12-05 22:15:47 +0100

    Update

--- main.py
+++ main.py
@@ -6,12 +6,12 @@ from flask import Flask, render_template
 polygon_coords = []
 # for i in range(4):
 #     address = input(f"Enter the {i+1}. address: ")
-
-address = ["Augustenstraße 92-94, 80798 München", "Augustenstraße 7, 80333 München", "Brienner Str. 11, 80333 München", "Schellingstraße 30, 80799 München"]
+# Augustenstraße 92-94, 80798 München
+address = ["Technical University of Munich", "Augustenstraße 7, 80333 München", "Brienner Str. 11, 80333 München", "Schellingstraße 30, 80799 München"]
 
 for i in address:
     polygon_coords.append(extracting_map.extract_lat_lng(i))
-
+    
 # finding center point of 4 coords 
 polygon = Polygon(polygon_coords) # a list
 poly_wkt = polygon.wkt
